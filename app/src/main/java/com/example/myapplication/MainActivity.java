ddec9e849c50ad724cf801eb535c6a509a89606e
woxing <zhan0545@algonquinlive.com>
2020-10-13 23:17:16 -0400

    Task #29 Complete, Partially started Task #27

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,97 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * public Strings to make it easier to fetch intents from Questions page
+     */
+    public static final String DISABILITY_TYPE = "disabilityType";
+    public static final String VISION = "vision";
+    public static final String SPEAKING = "speaking";
+    public static final String HEARING = "hearing";
+    public static final String WALKING  = "walking";
+    public static final String ELIMINATING = "eliminating";
+    public static final String FEEDING = "feeding";
+    public static final String DRESSING = "dressing";
+    public static final String MENTAL = "mental";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Adds the click functions for the Vision button
+        ImageButton vision = findViewById(R.id.imagevision);
+        vision.setOnClickListener( e -> addClickFunction(VISION));
+
+        //Adds click functions for the Speaking Button
+        ImageButton speaking = findViewById(R.id.imagespeaking);
+        speaking.setOnClickListener( e -> addClickFunction(SPEAKING));
+
+        //Adds click functions for the Hearing Button
+        ImageButton hearing = findViewById(R.id.imagehearing);
+        hearing.setOnClickListener( e -> addClickFunction(HEARING));
+
+        //Adds click functions for the Walking Button
+        ImageButton walking = findViewById(R.id.imagewalking);
+        walking.setOnClickListener( e -> addClickFunction(WALKING));
+
+        //Adds click functions for the Eliminating Button
+        ImageButton eliminating = findViewById(R.id.imageeliminating);
+        eliminating.setOnClickListener( e -> addClickFunction(ELIMINATING));
+
+        //Adds click functions for the Feeding Button
+        ImageButton feeding = findViewById(R.id.imagefeeding);
+        feeding.setOnClickListener( e -> addClickFunction(FEEDING));
+
+        //Adds click functions for the Dressing Button
+        ImageButton dressing = findViewById(R.id.imagedressing);
+        dressing.setOnClickListener( e -> addClickFunction(DRESSING));
+
+        //Adds click functions for the Mental Button
+        ImageButton mental = findViewById(R.id.imagemental);
+        mental.setOnClickListener( e -> addClickFunction(MENTAL));
+    }
+
+    /**
+     * When an image button is clicked, it will pass the type in the form of a String to this method
+     * It will start a new intent to the QuestionActivity while passing over the disability type clicked
+     * @param type String which contains the type of disability that image button represents
+     */
+    public void addClickFunction(String type){
+        Intent goToQuestions = new Intent(MainActivity.this,QuestionActivity.class);
+        switch(type) {
+            case SPEAKING:
+                goToQuestions.putExtra(DISABILITY_TYPE,SPEAKING);
+                break;
+            case HEARING:
+                goToQuestions.putExtra(DISABILITY_TYPE,HEARING);
+                break;
+            case WALKING:
+                goToQuestions.putExtra(DISABILITY_TYPE,WALKING);
+                break;
+            case ELIMINATING:
+                goToQuestions.putExtra(DISABILITY_TYPE,ELIMINATING);
+                break;
+            case FEEDING:
+                goToQuestions.putExtra(DISABILITY_TYPE,FEEDING);
+                break;
+            case DRESSING:
+                goToQuestions.putExtra(DISABILITY_TYPE,DRESSING);
+                break;
+            case MENTAL:
+                goToQuestions.putExtra(DISABILITY_TYPE,MENTAL);
+                break;
+            default: //Vision will act as default
+                goToQuestions.putExtra(DISABILITY_TYPE,VISION);
+                break;
+        }
+        startActivity(goToQuestions);
     }
 }
--- app/src/main/java/com/example/myapplication/QuestionActivity.java
+++ app/src/main/java/com/example/myapplication/QuestionActivity.java
@@ -2,24 +2,35 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class QuestionActivity extends AppCompatActivity {
+
+    private String disabilityType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
 
-        //Temporary radioGroup which holds a temporary question
+        //Loads the type of disability passed from the previous page
+        loadIntent();
+        loadImage();
+        //Loads the questions based on the disability
+        loadQuestions();
+
+        //Temporary radioGroup which holds a default question that will appear on every disability
         RadioGroup radioGroup = findViewById(R.id.defaultRadio);
 
         //Submit button which will process the answers
@@ -34,16 +45,91 @@ public class QuestionActivity extends AppCompatActivity {
     }
 
     /**
+     * Loads the intent from the main page and sets the global variable disabilityType to that intent
+     */
+    private void loadIntent() {
+        Intent fromMain = getIntent();
+        disabilityType = fromMain.getStringExtra(MainActivity.DISABILITY_TYPE);
+    }
+
+    /**
+     * Loads the Icon at the top of the page based on the selected disability
+     */
+    private void loadImage() {
+        //This is the icon we will change depending on the disability type loaded...
+        ImageView image = findViewById(R.id.selectedIcon);
+
+        switch(disabilityType) {
+            case MainActivity.SPEAKING:
+                image.setImageResource(R.drawable.speaking);
+                break;
+            case MainActivity.HEARING:
+                image.setImageResource(R.drawable.hearing);
+                break;
+            case MainActivity.WALKING:
+                image.setImageResource(R.drawable.walking);
+                break;
+            case MainActivity.ELIMINATING:
+                image.setImageResource(R.drawable.eliminating);
+                break;
+            case MainActivity.FEEDING:
+                image.setImageResource(R.drawable.feeding);
+                break;
+            case MainActivity.DRESSING:
+                image.setImageResource(R.drawable.dressing);
+                break;
+            case MainActivity.MENTAL:
+                image.setImageResource(R.drawable.mental);
+                break;
+            default: //default case will act like Vision
+                image.setImageResource(R.drawable.vision);
+                break;
+            //TODO: Create cases for every disability, changing the icon of ImageView accordingly.
+        }
+    }
+
+    /**
+     * Loads the questions based on the disability selected
+     */
+    private void loadQuestions() {
+        //The parent view questions will be populated by the child view based on disabilityType
+        LinearLayout questions = findViewById(R.id.questions);
+        View questionView;
+
+        switch(disabilityType) {
+            case MainActivity.SPEAKING:
+                break;
+            case MainActivity.HEARING:
+                break;
+            case MainActivity.WALKING:
+                break;
+            case MainActivity.ELIMINATING:
+                break;
+            case MainActivity.FEEDING:
+                break;
+            case MainActivity.DRESSING:
+                break;
+            case MainActivity.MENTAL:
+                break;
+            default: //default case will act like Vision
+                questionView = getLayoutInflater().inflate(R.layout.questions_vision, null);
+                questions.addView(questionView);
+                break;
+        }
+    }
+
+
+    /**
      * TODO: Implement a submit button on the questions
      */
-    protected void submitData() {
+    private void submitData() {
 
     }
 
     /**
      * Implements OnSeekBarChangedListener methods to track seekbar data
      */
-    protected void setSeekBarActions(SeekBar seekbar) {
+    private void setSeekBarActions(SeekBar seekbar) {
         //TextView which displays selection 1-10 from seekbar
         TextView data = findViewById(R.id.seekBarData);
         seekbar.setOnSeekBarChangeListener( new SeekBar.OnSeekBarChangeListener() {
