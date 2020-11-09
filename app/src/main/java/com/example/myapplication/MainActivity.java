2680ac82c67fbf70caaa4e17b83e74e55ed4f854
woxing <zhan0545@algonquinlive.com>
2020-11-08 19:10:03 -0500

    #61 - Uncluttered the main activity by making it a menu with buttons. Created a new DisabilitySelectionActivity and cleaned up some code.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,19 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
-import java.util.Calendar;
-import android.widget.AdapterView;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,30 +25,60 @@ public class MainActivity extends AppCompatActivity {
     public static final String FEEDING = "feeding";
     public static final String DRESSING = "dressing";
     public static final String MENTAL = "mental";
+    public static final String DATE = "date";
+    public static final String USER = "user";
 
-    private TextView dateText;
-    private String default_date;
+    /**
+     * Button which goes to disability selection page
+     */
+    private Button createEntry;
 
+    /**
+     * Button which goes to list containing individual entries
+     */
+    private Button viewIndividual;
+
+    /**
+     * Button which goes to page with summary of entries
+     */
+    private Button viewSummary;
+
+    /**
+     * Intent for going to next page
+     */
+    private Intent goToActivity;
+
+    /**
+     * On create for Main Activity should simply initialize buttons and implement action listeners
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dateText = findViewById(R.id.dateText);
-        setDefaultDate();
+        createEntry = findViewById(R.id.entryButton);
+        viewIndividual = findViewById(R.id.listViewButton);
+        viewSummary = findViewById(R.id.summaryButton);
 
-//        Button summary = findViewById(R.id.summaryButton);
-//        summary.setOnClickListener( e-> {
-//            Intent goToSummary = new Intent(MainActivity.this, SummaryActivity.class);
-//            startActivity(goToSummary);
-//        });
+        createEntry.setOnClickListener(e -> {
+            goToActivity = new Intent(MainActivity.this, DisabilitySelectionActivity.class);
+            startActivity(goToActivity);
+        });
+
+        viewIndividual.setOnClickListener(e -> {
+            goToActivity = new Intent(MainActivity.this,ListViewActivity.class);
+            startActivity(goToActivity);
+        });
+
+        viewSummary.setOnClickListener(e -> {
+            goToActivity = new Intent(MainActivity.this, SummaryActivity.class);
+            startActivity(goToActivity);
+        });
 
         //Spinner element
         Spinner spinner = (Spinner) findViewById(R.id.summaryButton_spinner);
 
         //Spinner click listener
-       // spinner.setOnItemClickListener((AdapterView.OnItemClickListener) this);
-
         //Spinner Drop down elements
         List<String> options = new ArrayList<String>();
         options.add("Bi-Weekly Summary");
@@ -70,128 +93,5 @@ public class MainActivity extends AppCompatActivity {
 
         //Apply the adapter to the spinner
         spinner.setAdapter(dataAdapter);
-
-
-
-        Button listButton = findViewById(R.id.listViewButton);
-        listButton.setOnClickListener( e-> {
-            Intent goToSummary = new Intent(MainActivity.this, ListViewActivity.class);
-            startActivity(goToSummary);
-        });
-
-        //Adds the click functions for the Calendar button
-        ImageButton calendar = findViewById(R.id.imagecalendar);
-        calendar.setOnClickListener( e -> showCalendar());
-
-        //Adds the click functions for the Vision button
-        ImageButton vision = findViewById(R.id.imagevision);
-        vision.setOnClickListener( e -> addClickFunction(VISION));
-
-        //Adds click functions for the Speaking Button
-        ImageButton speaking = findViewById(R.id.imagespeaking);
-        speaking.setOnClickListener( e -> addClickFunction(SPEAKING));
-
-        //Adds click functions for the Hearing Button
-        ImageButton hearing = findViewById(R.id.imagehearing);
-        hearing.setOnClickListener( e -> addClickFunction(HEARING));
-
-        //Adds click functions for the Walking Button
-        ImageButton walking = findViewById(R.id.imagewalking);
-        walking.setOnClickListener( e -> addClickFunction(WALKING));
-
-        //Adds click functions for the Eliminating Button
-        ImageButton eliminating = findViewById(R.id.imageeliminating);
-        eliminating.setOnClickListener( e -> addClickFunction(ELIMINATING));
-
-        //Adds click functions for the Feeding Button
-        ImageButton feeding = findViewById(R.id.imagefeeding);
-        feeding.setOnClickListener( e -> addClickFunction(FEEDING));
-
-        //Adds click functions for the Dressing Button
-        ImageButton dressing = findViewById(R.id.imagedressing);
-        dressing.setOnClickListener( e -> addClickFunction(DRESSING));
-
-        //Adds click functions for the Mental Button
-        ImageButton mental = findViewById(R.id.imagemental);
-        mental.setOnClickListener( e -> addClickFunction(MENTAL));
-    }
-
-    /**
-     * When an image button is clicked, it will pass the type in the form of a String to this method
-     * It will start a new intent to the QuestionActivity while passing over the disability type clicked
-     * @param type String which contains the type of disability that image button represents
-     */
-    public void addClickFunction(String type){
-        Intent goToQuestions = new Intent(MainActivity.this,QuestionActivity.class);
-        switch(type) {
-            case SPEAKING:
-                goToQuestions.putExtra(DISABILITY_TYPE,SPEAKING);
-                goToQuestions.putExtra("id",2);
-                break;
-            case HEARING:
-                goToQuestions.putExtra(DISABILITY_TYPE,HEARING);
-                goToQuestions.putExtra("id",3);
-                break;
-            case WALKING:
-                goToQuestions.putExtra(DISABILITY_TYPE,WALKING);
-                goToQuestions.putExtra("id",4);
-                break;
-            case ELIMINATING:
-                goToQuestions.putExtra(DISABILITY_TYPE,ELIMINATING);
-                goToQuestions.putExtra("id",5);
-                break;
-            case FEEDING:
-                goToQuestions.putExtra(DISABILITY_TYPE,FEEDING);
-                goToQuestions.putExtra("id",6);
-                break;
-            case DRESSING:
-                goToQuestions.putExtra(DISABILITY_TYPE,DRESSING);
-                goToQuestions.putExtra("id",7);
-                break;
-            case MENTAL:
-                goToQuestions.putExtra(DISABILITY_TYPE,MENTAL);
-                goToQuestions.putExtra("id",8);
-                break;
-            default: //Vision will act as default
-                goToQuestions.putExtra(DISABILITY_TYPE,VISION);
-                goToQuestions.putExtra("id",1);
-                break;
-        }
-        goToQuestions.putExtra("DATE", default_date);
-        startActivity(goToQuestions);
-    }
-
-    /**
-     * Sets the default date of the calendar
-     */
-    public void setDefaultDate() {
-
-        final Calendar cldr = Calendar.getInstance();
-
-        // Get the current day, month, year
-        int day = cldr.get(Calendar.DAY_OF_MONTH);
-        int month = cldr.get(Calendar.MONTH);
-        int year = cldr.get(Calendar.YEAR);
-        default_date = day+"/"+month+"/"+year;
-        dateText.setText(default_date);
-    }
-
-
-    public void showCalendar() {
-        DatePickerDialog picker;
-        final Calendar cldr = Calendar.getInstance();
-
-        // Get the current day, month, year
-        int day = cldr.get(Calendar.DAY_OF_MONTH);
-        int month = cldr.get(Calendar.MONTH);
-        int year = cldr.get(Calendar.YEAR);
-
-        // Create a calendar dialog for user to select the date, today's date is the default
-        picker = new DatePickerDialog(MainActivity.this, (datePicker, calYear, calMonth, calDate) -> {
-            // Set the date to the user's selection
-            default_date = calDate + "/" + (calMonth + 1) + "/" + calYear;
-            dateText.setText(default_date);
-        }, year, month, day);
-        picker.show();
     }
 }
--- app/src/main/java/com/example/myapplication/QuestionActivity.java
+++ app/src/main/java/com/example/myapplication/QuestionActivity.java
@@ -3,174 +3,174 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.Icon;
-import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
-import android.widget.RadioButton;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
-import java.util.Queue;
 
 public class QuestionActivity extends AppCompatActivity {
 
+    /**
+     * String which will hold the name of the disability the page is based on
+     */
     private String disabilityType;
-    DBHelper QuestionsDB;
-    SeekBar bar;
-    RadioGroup radioGroup, radioGroup1, radioGroup2;
-    RadioButton radioBtn1;
-    RadioButton radioBtn2, radioBtn3, radioBtn4, radioBtn5, radioBtn6;
-    boolean isChecked, isSelected;
-    private int id;
-    public static final String DATE_FORMAT = "dd MMMM yyyy";
-    public static final String TIME_FORMAT = "hh:mm a";
-    private String default_date;
 
+    /**
+     * SeekBar which allows user to drag
+     */
+    private SeekBar bar;
 
+    /**
+     * The TextView stores the value of the SeekBars current location (1-10 in string format)
+     */
+    private TextView seekBarData;
+
+    /**
+     * The ImageView at the top of the activity which should display the disability icon
+     */
+    private ImageView displayIcon;
+
+    /**
+     * The button that will do submit functions when clicked
+     */
+    private Button submit;
+
+    /**
+     * The date passed over from the selection page which is fetched by intent
+     */
+    private String entryDate;
+
+    /**
+     * Stores the intent passed from the selection screen. Holds date, and disability type
+     */
+    private Intent fromSelection;
+
+    /**
+     * Holds the default SeekBar value
+     */
+    private static final String DEFAULT_SEEK_VALUE = "0";
+
+    /**
+     * LinearLayout which is a placeholder for a questions that will be loaded in
+     */
+    private LinearLayout questions;
+
+    /**
+     * Database opener
+     */
+    private PrototypeOneDBOpener opener;
+
+    /**
+     * SQLiteDatabase
+     */
+    private SQLiteDatabase db;
+
+    /**
+     * onCreate does
+     * 1) Initializes values
+     * 2) Declares a SeekBar and set the value to 0.  This will  be contained in every question regardless of selection
+     * 3) Adds functionality to the submit button when clicked
+     * 4) Loads the intent
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
 
-        //Loads the type of disability passed from the previous page
-        loadIntent();
+        //1) Initialization
+        displayIcon = findViewById(R.id.selectedIcon);
+        questions = findViewById(R.id.questions);
+        opener = new PrototypeOneDBOpener(this);
+        db = opener.getWritableDatabase();
 
-        radioGroup = findViewById(R.id.defaultRadio);
-        radioGroup1 = findViewById(R.id.Vision1);
-        radioGroup2 = findViewById(R.id.Vision2);
-        radioBtn1 = findViewById(R.id.questionOneA);
-        radioBtn2 = findViewById(R.id.questionOneB);
-        radioBtn3 = findViewById(R.id.button1);
-        radioBtn4 = findViewById(R.id.button2);
-        radioBtn5 = findViewById(R.id.button3);
-        radioBtn6 = findViewById(R.id.button4);
-        QuestionsDB = new DBHelper(this);
-        //Seekbar for selecting severity 1-10
+        //2) Declare a SeekBar and implement actions. Set SeekBar value to 0 by default
         bar = findViewById(R.id.seekBar);
         setSeekBarActions(bar);
-        TextView bar = findViewById(R.id.seekBarData);
-        bar.setText("0");
+        seekBarData = findViewById(R.id.seekBarData);
+        seekBarData.setText(DEFAULT_SEEK_VALUE);
 
-        //Submit button which will process the answers
-        Button submit = findViewById(R.id.button);
-        submit.setOnClickListener(e -> {
-            submitPrototype();
-            //submitData();
-            //addClickFunction();
-        });
+        //3) Adds an OnClickListener to the submit button
+        submit = findViewById(R.id.button);
+        submit.setOnClickListener(e -> submitPrototype());
 
-
-    }
-    public static String getCurrentDate() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
-        //dateFormat.setTimeZone(TimeZone.getTimeZone("UTC+5"));
-        Date today = Calendar.getInstance().getTime();
-        return dateFormat.format(today);
+        //4) Loads intent
+        fromSelection = getIntent();
+        loadIntent();
     }
 
-    public static String getCurrentTime() {
-        SimpleDateFormat timeFormat = new SimpleDateFormat(TIME_FORMAT);
-        //dateFormat.setTimeZone(TimeZone.getTimeZone("UTC+5"));
-        Date today = Calendar.getInstance().getTime();
-        return timeFormat.format(today);
-    }
-    public void addClickFunction() {
-        Intent goToQuestions = new Intent(QuestionActivity.this, SummaryActivity.class);
-        startActivity(goToQuestions);
-    }
     /**
-     * Loads the intent from the main page and sets the global variable disabilityType to that intent
-     * Using a decision structure it chooses the appropriate image and the questions based on the disabilityType
+     * Loads the intent from the main page. The following things are processed in this method
+     * Icon, Date, Questions
      */
     private void loadIntent() {
-        //Intent passed from the main page
-        Intent fromMain = getIntent();
-        //ImageView for the icon on the top of the questions page
-        ImageView image = findViewById(R.id.selectedIcon);
+        disabilityType = fromSelection.getStringExtra(MainActivity.DISABILITY_TYPE);
+        entryDate = fromSelection.getStringExtra(MainActivity.DATE);
 
+        //The only reason the values should be null is during testing, however null cases set today, and vision to default
+        if(disabilityType == null) disabilityType = MainActivity.VISION;
+        if(entryDate == null) entryDate =
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + "/" + (Calendar.getInstance().get(Calendar.MONTH)+1) + "/" + Calendar.getInstance().get(Calendar.YEAR);
 
-        //Fetches the string from the Intent. If it's null it will be vision by default
-        disabilityType = (fromMain.getStringExtra(MainActivity.DISABILITY_TYPE) == null) ? MainActivity.VISION : fromMain.getStringExtra(MainActivity.DISABILITY_TYPE);
-        //Fetches the default date
-        default_date = (fromMain.getStringExtra("DATE") == null) ? "10/10/2020" : fromMain.getStringExtra("DATE");
-
-        if(fromMain.getStringExtra(MainActivity.DISABILITY_TYPE) == null) {
-            disabilityType = MainActivity.VISION;
-            id = 1;
-        }
-        else {
-            disabilityType = fromMain.getStringExtra(MainActivity.DISABILITY_TYPE);
-            id = fromMain.getIntExtra("id", 0);
-        }
-
-        //LinearLayout questions will hold the questions from specific disability layout files
-        LinearLayout questions = findViewById(R.id.questions);
+        Log.d("loadIntent date",entryDate );
+        Log.d("loadIntent disability", disabilityType);
 
         //initialize default layout file to be vision
         int defaultQuestion = R.layout.questions_vision;
 
+        //Loads icon and question layout based on disability type
         switch (disabilityType) {
             case MainActivity.SPEAKING:
-                image.setImageResource(R.drawable.speaking);
+                displayIcon.setImageResource(R.drawable.speaking);
                 defaultQuestion = R.layout.questions_speaking;
                 break;
             case MainActivity.HEARING:
-                image.setImageResource(R.drawable.hearing);
+                displayIcon.setImageResource(R.drawable.hearing);
                 defaultQuestion = R.layout.questions_hearing;
                 break;
             case MainActivity.WALKING:
-                image.setImageResource(R.drawable.walking);
+                displayIcon.setImageResource(R.drawable.walking);
                 defaultQuestion = R.layout.questions_walking;
                 break;
             case MainActivity.ELIMINATING:
-                image.setImageResource(R.drawable.eliminating);
+                displayIcon.setImageResource(R.drawable.eliminating);
                 defaultQuestion = R.layout.questions_eliminating;
                 break;
             case MainActivity.FEEDING:
-                image.setImageResource(R.drawable.feeding);
+                displayIcon.setImageResource(R.drawable.feeding);
                 defaultQuestion = R.layout.questions_feeding;
                 break;
             case MainActivity.DRESSING:
-                image.setImageResource(R.drawable.dressing);
+                displayIcon.setImageResource(R.drawable.dressing);
                 defaultQuestion = R.layout.questions_dressing;
                 break;
             case MainActivity.MENTAL:
-                image.setImageResource(R.drawable.mental);
+                displayIcon.setImageResource(R.drawable.mental);
                 defaultQuestion = R.layout.questions_mental;
                 break;
             default: //default case will act like Vision
-                image.setImageResource(R.drawable.vision);
+                displayIcon.setImageResource(R.drawable.vision);
                 break;
         }
-        View questionView = getLayoutInflater().inflate(defaultQuestion, null);
-        questions.addView(questionView);
+        //Adds the question layout to questions
+        questions.addView(getLayoutInflater().inflate(defaultQuestion, null));
     }
 
     /**
-     * Temporary submit function created to suit the purposes of Prototype 1
      * Saves the disabilityType, severity, and date
      */
     private void submitPrototype() {
-
         TextView bar = findViewById(R.id.seekBarData);
-        if(disabilityType != null && bar.getText() != null && default_date != null) {
-            PrototypeOneDBOpener opener = new PrototypeOneDBOpener(this);
-            SQLiteDatabase db = opener.getWritableDatabase();
+        if(disabilityType != null && bar.getText() != null && entryDate != null) {
             int severity = Integer.parseInt(bar.getText().toString());
-            opener.insert(db, disabilityType, severity, default_date);
+            opener.insert(db, disabilityType, severity, entryDate);
             startActivity(new Intent(QuestionActivity.this, SummaryActivity.class));
             Toast.makeText(this, "Entry Saved", Toast.LENGTH_LONG).show();
         } else {
@@ -179,68 +179,10 @@ public class QuestionActivity extends AppCompatActivity {
 
     }
 
-
     /**
-     * submitData written by Sirmanjit
-     */
-    private void submitData() {
-        if (id == 1) {
-            int selectedId = radioGroup.getCheckedRadioButtonId();
-            int selectedId1 = radioGroup1.getCheckedRadioButtonId();
-            int selectedId2 = radioGroup2.getCheckedRadioButtonId();
-
-            // find the radiobutton by returned id
-            radioBtn1 = (RadioButton) findViewById(selectedId);
-            radioBtn3 = (RadioButton) findViewById(selectedId1);
-            radioBtn5 = (RadioButton) findViewById(selectedId2);
-            TextView seekTextView = findViewById(R.id.seekBarData);
-            String firstRadioButtonString = radioBtn1.getText().toString();
-            int seekBarData;
-            if (seekTextView.getText().toString().equals("")) {
-                seekBarData = 0;
-            } else {
-                seekBarData = Integer.parseInt(seekTextView.getText().toString());
-
-            }
-            String secondRadioButtonString = radioBtn3.getText().toString();
-            String thirdRadioButtonString = radioBtn5.getText().toString();
-            String data = firstRadioButtonString + "," + seekBarData + "," + secondRadioButtonString + "," + thirdRadioButtonString;
-            Boolean result = QuestionsDB.addQuestionData(data, id,getCurrentDate()+" "+getCurrentTime());
-            if (result) {
-                Toast.makeText(this, "Inserted Values are " + firstRadioButtonString + "," + seekBarData + "," + secondRadioButtonString + "," + thirdRadioButtonString+" "+getCurrentDate()+" "+getCurrentTime(), Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(QuestionActivity.this, SummaryActivity.class));
-            }
-        } else {
-            int selectedId = radioGroup.getCheckedRadioButtonId();
-            // find the radiobutton by returned id
-            radioBtn1 = (RadioButton) findViewById(selectedId);
-
-            TextView seekTextView = findViewById(R.id.seekBarData);
-            String firstRadioButtonString = radioBtn1.getText().toString();
-            int seekBarData;
-            if (seekTextView.getText().toString().equals("")) {
-                seekBarData = 0;
-            } else {
-                seekBarData = Integer.parseInt(seekTextView.getText().toString());
-
-            }
-            Boolean result = QuestionsDB.addQuestionData(firstRadioButtonString + "," + seekBarData, id, getCurrentDate() + " " + getCurrentTime());
-            if (result) {
-                Toast.makeText(this, "Inserted Values are " + firstRadioButtonString + "," + seekBarData + " " + getCurrentDate() + " " + getCurrentTime(), Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(QuestionActivity.this, SummaryActivity.class));
-            }
-        }
-
-    }
-
-
-
-    /**
-     * Implements OnSeekBarChangedListener methods to track seekbar data
+     * Implements OnSeekBarChangedListener methods to track SeekBar data
      */
     private void setSeekBarActions(SeekBar seekbar) {
-        //TextView which displays selection 1-10 from seekbar
-        TextView data = findViewById(R.id.seekBarData);
         seekbar.setOnSeekBarChangeListener( new SeekBar.OnSeekBarChangeListener() {
 
             @Override
@@ -249,15 +191,12 @@ public class QuestionActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                //Not needed
-            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
                 String display = String.valueOf(progress/10);
-                data.setText(display);
-
+                seekBarData.setText(display);
             }});
     }
 }
