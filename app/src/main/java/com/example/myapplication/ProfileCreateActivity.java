a70f7a934ffffe4c1a7bc403dd82b07c1b4fe520
lee5691 <leeyuri21@hotmai.com>
2020-11-15 07:30:07 -0500

    #70 created ProfileAcreateActivity layout

--- app/src/main/java/com/example/myapplication/ProfileCreateActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileCreateActivity.java
@@ -2,13 +2,61 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ProfileCreateActivity extends AppCompatActivity {
+
+    EditText et_first, et_last;
+    Button bt_create2;
+    SharedPreferences prefs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_create);
+
+        et_first = findViewById(R.id.create_editText_1);
+        et_last = findViewById(R.id.create_editText_2);
+
+        prefs = getSharedPreferences("profileData", Context.MODE_PRIVATE);
+
+        String first = prefs.getString("first","");
+        String last = prefs.getString("last","");
+        et_first.setText(first);
+        et_last.setText(last);
+
+        bt_create2 = findViewById(R.id.create2_btn);
+        bt_create2.setOnClickListener(v -> {
+            String firstName = et_first.getText().toString();
+            String lastName = et_last.getText().toString();
+
+            if (firstName.trim().isEmpty()) {
+                Toast.makeText(this, "First Name should be entered", Toast.LENGTH_LONG).show();
+            } else if (lastName.trim().isEmpty()) {
+                Toast.makeText(this, "Last Name should be entered", Toast.LENGTH_LONG).show();
+            } else {
+                Intent goToFavourite = new Intent(this, MainActivity.class);
+                startActivity(goToFavourite);
+            }
+
+        });
     }
+
+    /**
+     *Need to be updated with database
+     */
+    protected void onPause(String first, String last) {
+        prefs.edit()
+                .putString("first", first)
+                .putString("last", last)
+                .apply();
+    }
+
 }
--- app/src/main/java/com/example/myapplication/ProfileListActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileListActivity.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 
 public class ProfileListActivity extends AppCompatActivity {
 
+    /**
+     * The stored profiles list (NEED TO BE UPDATED W/ DATABASE)
+     */
+    public static final String SELECTED_FIRST = "FIRST";
+    public static final String SELECTED_LAST = "LAST";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
