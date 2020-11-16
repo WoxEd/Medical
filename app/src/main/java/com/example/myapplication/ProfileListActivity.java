8c2664a7a6464789b1fed584448efd0ea075e5ad
lee5691 <leeyuri21@hotmai.com>
2020-11-16 01:37:56 -0500

    #70 created PrlfileListActivity layout

--- app/src/main/java/com/example/myapplication/ProfileListActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileListActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ProfileListActivity extends AppCompatActivity {
 
@@ -12,9 +14,30 @@ public class ProfileListActivity extends AppCompatActivity {
     public static final String SELECTED_FIRST = "FIRST";
     public static final String SELECTED_LAST = "LAST";
 
+    ListView listView;
+    Bundle dataToPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_list);
+
+        listView = findViewById(R.id.listView);
+
+        listView.setOnItemClickListener((list, view, position,id) -> {
+            dataToPass = new Bundle();
+            /**
+             * ProfileList required using database
+             */
+//            ProfileList profileList = (ProfileList) listView.getAdapter().getItem(position);
+//            dataToPass.putString(SELECTED_FIRST, pList.getFirst());
+//            dataToPass.putString(SELECTED_LAST, pList.getLast());
+//            ...
+
+            Intent nextActivity = new Intent(ProfileListActivity.this, MainActivity.class);
+            nextActivity.putExtras(dataToPass);
+            startActivity(nextActivity);
+
+        });
     }
 }
